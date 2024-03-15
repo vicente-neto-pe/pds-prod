8899020606808cb85f8cf3227c868e7fba1f1cfd
vicente-neto-pe <158325853+vicente-neto-pe@users.noreply.github.com>
2024-03-15 12:04:48 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  
   return (
-    <div>Home</div>
+    <div>Home
+
+      <button onClick={()=>navigate('/profile')}>Navigate to profile</button>
+    </div>
   )
 }
